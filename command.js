6546aaec9cda7d589fc8763421349e896b2148d6
Travis <azai392@gmail.com>
2024-02-27 02:27:11 -0600

    Message class done and tests passed!

--- command.js
+++ command.js
@@ -1,7 +1,7 @@
 class Command {
    constructor(commandType, value) {
      this.commandType = commandType; // commandType will be one of the following: MODE_CHANGE, MOVE, or STATUS_CHECK
-     if (!commandType) {
+     if (!commandType) { 
        throw Error("Command type required.");
      }
      this.value = value; // related to the type of commmand
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,4 +6,19 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
+    it("throws error if a name is NOT passed into constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Message name required.'));
+      });
+
+    it("constructor sets name", function () {
+        let testName = new Message('Testing Message Property');
+        expect(testName.name).toBe('Testing Message Property');
+    });
+
+    it("contains a command array passed into the constructor as the 2nd argument", function () {
+        let testCommandsArr = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command ('MOVE', 1234)];
+        let testMessage = new Message("Test Message", testCommandsArr);
+        expect(testMessage.commands).toBe(testCommandsArr); 
+    });
+
 });
